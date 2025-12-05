8ff5940f6a3c6c0102da4ac1fbca107ca09f55cd
Juan Pablo F. H. <20206470@garzas.uatx.mx>
2025-12-04 22:12:48 -0600

    Modificaciones input

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'SimuladorCuidadoRecienNacidoNYL',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    Keyboard: {
+      resize: "body",
+      style: "dark",
+      resizeOnFullScreen: true
+    }
+  }
 };
 
 export default config;
